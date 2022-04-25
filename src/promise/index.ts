212a6581d82da93608d0eef5c26a5835a6579458
GWJ <gaowujie.2004@bytedance.com>
2022-04-25 21:08:47 +0800

    feat: promise main api
    feat
    
    feat: promise  other api: Promise.resolve/reject
    
    fix: 注释
    
    feat: finally ok

---
+++ src/promise/index.ts
@@ -0,0 +1,277 @@
+// 穿透 - resolve
+// 输出 111
+// Promise.resolve(111)
+//   .then()
+//   .then((val) => console.log(val));
+
+// 失败的promise，第一个 then 没有对应的 callback 那就继续 throw error
+// 让第一个 then() 返回失败的promise
+// Promise.reject(222)
+//   .then()
+//   .then(undefined, (reason) => {});
+
+const enum State {
+  Pending = 'Pending',
+  Fulfilled = 'Fulfilled',
+  Rejected = 'Rejected',
+}
+
+// 负责解析 x 和 promise2 之间的关系 ， p1.then(oncallback)， oncallback 函数调用后的返回值是 x
+// 解析 promise2 和 x之间的关系 来判断promise2 走 resolve 还是  reject
+// resolve、reject 和 promise2 是一起的。
+function resolvePromise(promise2, x, resolve, reject) {
+  if (promise2 === x) {
+    /**
+     * 就像下面这种：.then callback 是异步执行，能拿到 p
+     * let p = Promise.resolve().then((val) => p);
+     */
+    throw new TypeError('hh');
+  }
+
+  // x 可能是个promise, 引用类型
+  if ((typeof x === 'object' && x !== null) || typeof x === 'function') {
+    let called = false;
+
+    // x.then 可能是个 getter 那就可能会报错
+    try {
+      let then = x.then; // 缓存出来，因为 x.then 可能是个 getter 可能会耗性能
+      // x is promise
+      if (typeof then === 'function') {
+        // todo: 费脑筋 start
+        then.call(
+          x, // then 内部有 this
+          (y) => {
+            // y 的类型是啥？ x promise 的值作为 promise2 的值
+            if (called) return;
+            called = true;
+            resolvePromise(promise2, y, resolve, reject);
+          },
+          (r) => {
+            if (called) return;
+            called = true;
+            reject(r);
+          }
+        );
+        // todo: 费脑筋 end
+      } else {
+        resolve(x);
+      }
+    } catch (err) {
+      if (called) return;
+      called = true;
+      reject(err);
+    }
+  } else {
+    // 基本值
+    resolve(x);
+  }
+}
+
+export class _Promise {
+  protected state = State.Pending;
+  protected result: any = undefined;
+
+  protected onFulfillCallbacks: Function[] = [];
+  protected onRejectCallbacks: Function[] = [];
+
+  constructor(execute: (resolve: (v: any) => void, reject: (r: any) => void) => void) {
+    const resolve = (value: any) => {
+      if (value instanceof _Promise) {
+        // todo: 费脑筋
+        return value.then(resolve, reject);
+        value.then(
+          (val) => {
+            resolve(val);
+          },
+          (reason) => {
+            reject(reason);
+          }
+        );
+      }
+      if (this.state === State.Pending) {
+        this.state = State.Fulfilled;
+        this.result = value;
+        this.onFulfillCallbacks.forEach((fn) => fn(this.result));
+      }
+    };
+    const reject = (reason: any) => {
+      // 调用失败，直接失败
+      if (this.state === State.Pending) {
+        this.state = State.Rejected;
+        this.result = reason;
+        this.onRejectCallbacks.forEach((fn) => fn(this.result));
+      }
+    };
+
+    try {
+      execute(resolve, reject);
+    } catch (err) {
+      reject(err);
+      console.log('--execute error', err);
+    }
+  }
+
+  // todo: YYDS
+  static resolve(val: any) {
+    if (val instanceof _Promise) {
+      return val;
+    }
+
+    return new _Promise((resolve) => {
+      resolve(val);
+    });
+  }
+
+  static reject(reason: any) {
+    return new _Promise((_, reject) => {
+      reject(reason);
+    });
+  }
+
+  /**
+   * then() 返回 Promise 实例，实例的状态和结果，
+   * let p = new Promise((resolve, reject) => {.....})
+   * let p2 = p.then( val => {...} )
+   *  p2 的状态由 回调函数返回值决定，回调函数返回值：
+   *    1、基本类型
+   *    2、promise 实例
+   * @return Promise
+   */
+  then(onFulfill?: (val: any) => any, onReject?: (reason: any) => any) {
+    // 穿透
+    onFulfill = typeof onFulfill === 'function' ? onFulfill : (v) => v;
+    onReject =
+      typeof onReject === 'function'
+        ? onReject
+        : (r) => {
+            throw r;
+          };
+    const promise2 = new _Promise((resolve, reject) => {
+      if (this.state === State.Pending) {
+        // todo: 这里是使用 val 还是 this.result
+        this.onFulfillCallbacks.push((val) => {
+          setTimeout(() => {
+            try {
+              const x = onFulfill(val);
+              resolvePromise(promise2, x, resolve, reject);
+            } catch (err) {
+              reject(err);
+            }
+          }, 0);
+        });
+        this.onRejectCallbacks.push((reason) => {
+          setTimeout(() => {
+            try {
+              const x = onReject(reason);
+              resolvePromise(promise2, x, resolve, reject);
+            } catch (err) {
+              reject(err);
+            }
+          }, 0);
+        });
+      }
+
+      /**
+       * p.then(val => {})，val 是 p.result
+       */
+      if (this.state === State.Fulfilled) {
+        setTimeout(() => {
+          try {
+            const x = onFulfill(this.result);
+            // 放入异步，因为 promise2
+            resolvePromise(promise2, x, resolve, reject);
+          } catch (err) {
+            reject(err);
+          }
+        }, 0);
+      }
+      if (this.state === State.Rejected) {
+        setTimeout(() => {
+          try {
+            const x = onReject(this.result);
+            resolvePromise(promise2, x, resolve, reject);
+          } catch (err) {
+            reject(err);
+          }
+        }, 0);
+      }
+    });
+
+    return promise2;
+  }
+
+  /**
+   * @return Promise 实例
+   * return Promise 状态由 onReject 决定，和 then 一样
+   */
+  catch(onReject: (reason: any) => any) {
+    // 不用再判断非法值，因为 then 内部会做处理
+    return this.then(undefined, onReject);
+  }
+
+  /**
+   * todo: 坑
+   * p.finally(......) 返回的是 p
+   * 如果这样那就有问题了，正确的 Promise，是会等待 finally callback 的promise
+   * 只不过不使用 finally callback return 的值
+   *
+   * Promise
+   *  .reject(55555)
+   *  .finally(() => {
+   *     return new Promise(r => {
+   *        setTimeout(r, 4000)
+   *     })
+   *   })
+   *  .catch(reason => console.log('catch --', reason))  // 输出 5555
+   */
+  finallyError(callback: () => void) {
+    this.then(
+      () => {
+        callback();
+      },
+      () => {
+        callback();
+      }
+    );
+
+    return this;
+  }
+
+  // p.finally(......) 返回的是 p
+  // this -> p
+  finally(callback: () => void) {
+    return this.then(
+      (val) => {
+        return Promise.resolve(callback()).then(() => val);
+      },
+      (reason) => {
+        return Promise.resolve(callback()).then(() => {
+          throw reason;
+        });
+      }
+    );
+  }
+}
+
+/**================================== Test **/
+let p = _Promise.resolve(_Promise.resolve(_Promise.resolve(9000)));
+
+//从右往左，执行的。
+// 打印 9000
+p.then((val) => {
+  console.log(val);
+});
+
+let p2 = _Promise.resolve(_Promise.reject(11111));
+p2.then(null, (reason) => {
+  console.log('p2-- reject', reason);
+});
+
+_Promise
+  .reject(55555)
+  .finally(() => {
+    return new Promise((r) => {
+      setTimeout(r, 4000);
+    });
+  })
+  .catch((reason) => console.log('catch --', reason)); // 输出 5555
---
+++ src/promise/then/through.ts
@@ -0,0 +1,194 @@
+// 穿透 - resolve
+// 输出 111
+// Promise.resolve(111)
+//   .then()
+//   .then((val) => console.log(val));
+
+// 失败的promise，第一个 then 没有对应的 callback 那就继续 throw error
+// 让第一个 then() 返回失败的promise
+// Promise.reject(222)
+//   .then()
+//   .then(undefined, (reason) => {});
+
+const enum State {
+  Pending = 'Pending',
+  Fulfilled = 'Fulfilled',
+  Rejected = 'Rejected',
+}
+
+// 负责解析 x 和 promise2 之间的关系 ， p1.then(oncallback)， oncallback 函数调用后的返回值是 x
+// 解析 promise2 和 x之间的关系 来判断promise2 走 resolve 还是  reject
+// resolve、reject 和 promise2 是一起的。
+function resolvePromise(promise2, x, resolve, reject) {
+  if (promise2 === x) {
+    /**
+     * 就像下面这种：.then callback 是异步执行，能拿到 p
+     * let p = Promise.resolve().then((val) => p);
+     */
+    throw new TypeError('hh');
+  }
+
+  // x 可能是个promise, 引用类型
+  if ((typeof x === 'object' && x !== null) || typeof x === 'function') {
+    let called = false;
+
+    // x.then 可能是个 getter 那就可能会报错
+    try {
+      let then = x.then; // 缓存出来，因为 x.then 可能是个 getter 可能会耗性能
+      // x is promise
+      if (typeof then === 'function') {
+        // todo: 费脑筋 start
+        then.call(
+          x, // then 内部有 this
+          (y) => {
+            // y 的类型是啥？ x promise 的值作为 promise2 的值
+            if (called) return;
+            called = true;
+            resolvePromise(promise2, y, resolve, reject);
+          },
+          (r) => {
+            if (called) return;
+            called = true;
+            reject(r);
+          }
+        );
+        // todo: 费脑筋 end
+      } else {
+        resolve(x);
+      }
+    } catch (err) {
+      if (called) return;
+      called = true;
+      reject(err);
+    }
+  } else {
+    // 基本值
+    resolve(x);
+  }
+}
+
+export class _Promise {
+  protected state = State.Pending;
+  protected result: any = undefined;
+
+  protected onFulfillCallbacks: Function[] = [];
+  protected onRejectCallbacks: Function[] = [];
+
+  constructor(execute: (resolve: (v: any) => void, reject: (r: any) => void) => void) {
+    const resolve = (value: any) => {
+      if (value instanceof _Promise) {
+        // todo: 费脑筋
+        return value.then(resolve, reject);
+        value.then(
+          (val) => {
+            resolve(val);
+          },
+          (reason) => {
+            reject(reason);
+          }
+        );
+      }
+      if (this.state === State.Pending) {
+        this.state = State.Fulfilled;
+        this.result = value;
+        this.onFulfillCallbacks.forEach((fn) => fn(this.result));
+      }
+    };
+    const reject = (reason: any) => {
+      // 调用失败，直接失败
+      if (this.state === State.Pending) {
+        this.state = State.Rejected;
+        this.result = reason;
+        this.onRejectCallbacks.forEach((fn) => fn(this.result));
+      }
+    };
+
+    try {
+      execute(resolve, reject);
+    } catch (err) {
+      reject(err);
+      console.log('--execute error', err);
+    }
+  }
+
+  /**
+   * then() 返回 Promise 实例，实例的状态和结果，
+   * let p = new Promise((resolve, reject) => {.....})
+   * let p2 = p.then( val => {...} )
+   *  p2 的状态由 回调函数返回值决定，回调函数返回值：
+   *    1、基本知识
+   *    2、promise 实例
+   * @return Promise
+   */
+  then(onFulfill?: (val: any) => any, onReject?: (reason: any) => any) {
+    // 穿透
+    onFulfill = typeof onFulfill === 'function' ? onFulfill : (v) => v;
+    onReject =
+      typeof onReject === 'function'
+        ? onReject
+        : (r) => {
+            throw r;
+          };
+    const promise2 = new _Promise((resolve, reject) => {
+      if (this.state === State.Pending) {
+        // todo: 这里是使用 val 还是 this.result
+        this.onFulfillCallbacks.push((val) => {
+          setTimeout(() => {
+            try {
+              const x = onFulfill(val);
+              resolvePromise(promise2, x, resolve, reject);
+            } catch (err) {
+              reject(err);
+            }
+          }, 0);
+        });
+        this.onRejectCallbacks.push((reason) => {
+          setTimeout(() => {
+            try {
+              const x = onReject(reason);
+              resolvePromise(promise2, x, resolve, reject);
+            } catch (err) {
+              reject(err);
+            }
+          }, 0);
+        });
+      }
+
+      /**
+       * p.then(val => {})，val 是 p.result
+       */
+      if (this.state === State.Fulfilled) {
+        setTimeout(() => {
+          try {
+            const x = onFulfill(this.result);
+            // 放入异步，因为 promise2
+            resolvePromise(promise2, x, resolve, reject);
+          } catch (err) {
+            reject(err);
+          }
+        }, 0);
+      }
+      if (this.state === State.Rejected) {
+        setTimeout(() => {
+          try {
+            const x = onReject(this.result);
+            resolvePromise(promise2, x, resolve, reject);
+          } catch (err) {
+            reject(err);
+          }
+        }, 0);
+      }
+    });
+
+    return promise2;
+  }
+}
+
+/**================================== Test **/
+let p = new _Promise((resolve, reject) => {
+  resolve(111);
+});
+
+p.then().then((val) => {
+  console.log('----', val);
+});
