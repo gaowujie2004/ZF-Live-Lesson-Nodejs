f6978051612b2c985f9caf0dde2287772376c5fd
GaoWuJie2004 <gaowujie.2004@qq.com>
2022-05-28 01:38:51 +0800

    require - 初步

---
+++ src/require/index.js
@@ -0,0 +1,7 @@
+const son1 = require('./son1');
+
+// tsconfig.json 控制 .ts 后缀的文件
+// jsconfig.json 控制 .js 后缀的文件
+// eslint.json env 仅仅是代码飘红不飘红，不控制 vscode 的代码提示功能
+
+// 一个相关，只能有 tsconfig.json、jsconfig.json 其中一个。
